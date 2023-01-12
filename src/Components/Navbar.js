8499b4a6ac3368e52e21bf4eb47e9d4d34dbaad2
Himanshu Soni <39999787+blaringblazehs@users.noreply.github.com>
2023-01-12 16:42:33 +0530

    Navbar done

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,15 +1,35 @@
 import "./navbar.css";
-import { GiHamburgerMenu } from "react-icons/gi";
+// import { GiHamburgerMenu } from "react-icons/gi";
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import logo from "../Images/intech-logo.png";
 const Navbar = () => {
-    const [menu, setMenu] = useState(false);
+    // const [menu, setMenu] = useState(false);
     return (
         <nav className="main-nav">
             <div className="logo">
-                <h2>INtech</h2>
+                <img src={logo}></img>
             </div>
-            <div className="menu"></div>
+            <div className="menu-links">
+                <ul>
+                    <li>
+                        <a href="#">Home</a>
+                    </li>
+                    <li>
+                        <a href="#">Services</a>
+                    </li>
+                    <li>
+                        <a href="#">About</a>
+                    </li>
+                    <li>
+                        <a href="#">Home</a>
+                    </li>
+                </ul>
+            </div>
+            <div className="login-signup">
+                <button className="log-in">Login</button>
+                <button className="sign-up">Sign Up</button>
+            </div>
         </nav>
     );
 };
